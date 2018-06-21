2f82ea81433464e6fb79a8d98486f74ca469f233
Sérgio Carvalho <sergio.carvalho@sooma.com>
2018-06-21 18:47:35 +0100

    Fill-in message when constructing UnexpectedTokenException

--- lib/sergiosgc/Text/Parser/UnexpectedTokenException.php
+++ lib/sergiosgc/Text/Parser/UnexpectedTokenException.php
@@ -6,6 +6,9 @@ class Text_Parser_UnexpectedTokenException extends Text_Parser_Exception
 {
     public function __construct($token, $state)
     {
+        parent::__construct(sprintf("Unexpected token %s(%s) on state %d", (string) $token->getId(), (string) $token->getValue(), $state));
+        $this->token = $token;
+        $this->state = $state;
     }
 }
 ?>
